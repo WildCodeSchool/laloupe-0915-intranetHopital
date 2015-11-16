fd53364ddaef360f36f11176233f32293aed95e7
webdev33 <dty.dev@gmail.com>
2015-11-16 19:26:41 +0100

    Add Entity Fonction, Add Entity Service and Fixtures service and function

---
+++ src/IuchBundle/Entity/Fonction.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace IuchBundle\Entity;
+
+/**
+ * Fonction
+ */
+class Fonction
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nom;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Fonction
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+}
+
