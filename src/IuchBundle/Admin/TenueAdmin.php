322eca29ac7e80426a8ebd64c9af77fe6e8174c0
Lucie Mannechez <lucie.mannechez@gmail.com>
2015-11-30 17:43:48 +0100

    Add CRUD tenue Add autocomplete user

---
+++ src/IuchBundle/Admin/TenueAdmin.php
@@ -0,0 +1,72 @@
+<?php
+namespace IuchBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class TenueAdmin extends Admin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('nom')
+            ->add('user','sonata_type_model_autocomplete', array(
+                'property' => array('firstname', 'lastname', 'username', 'service.nom'),
+                'minimum_input_length' => 2
+            ))
+            ->add('date_donnee', 'datetime')
+            ->add('nombre_donne', 'number')
+            ->add('date_rendu', 'datetime', array('required'=> false))
+            ->add('nombre_rendu', 'number', array('required' => false))
+        ;
+    }
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('nom')
+            ->add('user')
+            ->add('date_donnee')
+            ->add('nombre_donne')
+            ->add('date_rendu')
+            ->add('nombre_rendu')
+        ;
+    }
+    protected function configureListFields(ListMapper $listMapper)
+    {
+
+        $listMapper
+            ->addIdentifier('nom')
+            ->add('user')
+            ->add('date_donnee')
+            ->add('nombre_donne')
+            ->add('date_rendu')
+            ->add('nombre_rendu')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'edit' => array(),
+                    'show' => array(),
+                    'delete' => array()
+                )
+            ))
+        ;
+    }
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->with('Général')
+            ->add('user')
+            ->add('nom')
+            ->with('Entrée')
+            ->add('date_donnee')
+            ->add('nombre_donne')
+            ->with('Sortie')
+            ->add('date_rendu')
+            ->add('nombre_rendu')
+            ->end()
+        ;
+    }
+}
