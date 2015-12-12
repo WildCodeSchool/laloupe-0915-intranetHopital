74c1593071187311ac3cdc0f79ade82d4b774ffd
Lucie Mannechez <lucie.mannechez@gmail.com>
2015-12-12 16:30:12 +0100

    Test for delete service

--- src/Application/Sonata/AdminBundle/Tests/Controller/CRUDControllerTest.php
+++ src/Application/Sonata/AdminBundle/Tests/Controller/CRUDControllerTest.php
@@ -8,6 +8,16 @@ use Symfony\Component\HttpFoundation\Request;
 
 class CRUDControllerTest extends WebTestCase
 {
+    protected $id;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function __construct()
+    {
+        $this->id = uniqid();
+    }
 
     private function connection($client, $username, $password)
     {
@@ -26,27 +36,140 @@ class CRUDControllerTest extends WebTestCase
         return $crawler;
     }
 
-    public function testServiceForm()
+    public function createService($values = array())
     {
-        $client = static::createClient();
+        $id = $this->getId();
 
+        $client = static::createClient();
         $crawler = $this->connection($client, 'testAdmin', 'testAdmin');
-        $crawler = $client->request('GET', '/admin/iuch/service/create?uniqid=1201934');
+        $crawler = $client->request('GET', '/admin/iuch/service/create?uniqid='.$id);
         $this->assertTrue(200 === $client->getResponse()->getStatusCode());
 
         $this->assertEquals('Application\Sonata\AdminBundle\Controller\CRUDController::createAction', $client->getRequest()->attributes->get('_controller'));
 
-        $id = '1201934';
+        $form = $crawler->selectButton('btn_create_and_list')->form(array_merge(array(
+            $id.'[nom]'          => 'Service',
+            $id.'[email]'        => 'service@gmail.com',
+            $id.'[telephone]'    => '0606060606'
+        ), $values));
 
-        $form = $crawler->selectButton('Créer')->form();
-        // set some values
-        $form[$id.'[nom]']          = 'Service';
-        $form[$id.'[email]']        ='service@gmail.com';
-        $form[$id.'[telephone]']    = '0606060606';
+        $client->submit($form);
 
-        // submit the form
-        $crawler = $client->submit($form);
-        $this->assertEquals('Application\Sonata\AdminBundle\Controller\CRUDController::createAction', $client->getRequest()->attributes->get('_controller'));
+        return $client;
     }
 
+    public function testServiceValidCreate()
+    {
+        $client = $this->createService();
+        $client->followRedirect();
+
+        $this->assertEquals('Application\Sonata\AdminBundle\Controller\CRUDController::listAction', $client->getRequest()->attributes->get('_controller'));
+
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $query = $em->createQuery('SELECT count(s.id) from IuchBundle:Service s WHERE s.nom = :nom AND s.email = :email AND s.telephone = :telephone');
+        $query->setParameter('nom', 'Service');
+        $query->setParameter('email', 'service@gmail.com');
+        $query->setParameter('telephone', '0606060606');
+        $this->assertTrue(0 < $query->getSingleScalarResult());
+    }
+
+    public function testServiceInvalidCreate()
+    {
+        $id = $this->getId();
+
+        $client = $this->createService(array($id.'[email]' => 'service', $id.'[telephone]' => 'diojefoijo'));
+        $crawler = $client->getCrawler();
+
+        $this->assertEquals('Application\Sonata\AdminBundle\Controller\CRUDController::createAction',
+            $client->getRequest()->attributes->get('_controller'));
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Veuillez rentrer un email valide.")')->count());
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Veuillez rentrer un numéro de téléphone valide.")')->count());
+    }
+/*
+    public function testServiceEdit()
+    {
+        $id = $this->getId();
+
+        $client = $this->createService(array($id.'[nom]' => 'ploup'));
+        $client->followRedirect();
+
+        $crawler = $client->getCrawler();
+
+        $link = $crawler
+            ->filter('td:contains("ploup")')
+            ->siblings()
+            ->eq(4)
+            ->children()
+            ->children()
+            ->eq(0)
+            ->link();
+        ;
+
+        $crawler = $client->click($link);
+
+        $this->assertEquals('Application\Sonata\AdminBundle\Controller\CRUDController::editAction',
+            $client->getRequest()->attributes->get('_controller'));
+
+        /*$url = $client->getRequest()->getUri();
+        $parts = parse_url($url);
+        $idE = end(parse_str($parts['query']));*/
+
+
+        $form = $crawler->selectButton('btn_update_and_list')->form(array(
+            '[email]'        => 'service@gmail.com',
+            '[telephone]'    => '0606060606'
+        ));
+
+        $client->submit($form);
+
+        $this->assertEquals('Application\Sonata\AdminBundle\Controller\CRUDController::listAction', $client->getRequest()->attributes->get('_controller'));
+
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $query = $em->createQuery('SELECT count(s.id) from IuchBundle:Service s WHERE s.nom = :nom AND s.email = :email AND s.telephone = :telephone');
+        $query->setParameter('nom', 'ploup');
+        $query->setParameter('email', 'service@gmail.com');
+        $query->setParameter('telephone', '0606060606');
+        $this->assertTrue(0 < $query->getSingleScalarResult());
+    }*/
+
+    public function testDeleteService()
+    {
+        $client = $this->createService(array($this->getId().'[nom]' => 'plop'));
+        $client->followRedirect();
+
+        $crawler = $client->getCrawler();
+
+        $link = $crawler
+            ->filter('td:contains("plop")')
+            ->siblings()
+            ->eq(4)
+            ->children()
+            ->children()
+            ->eq(2)
+            ->link();
+        ;
+
+        // and click it
+        $crawler = $client->click($link);
+
+        $this->assertEquals('Application\Sonata\AdminBundle\Controller\CRUDController::deleteAction', $client->getRequest()->attributes->get('_controller'));
+
+        $form = $crawler->selectButton('Oui, supprimer')->form();
+        $client->submit($form);
+
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $query = $em->createQuery('SELECT count(s.id) from IuchBundle:Service s WHERE s.nom = :nom');
+        $query->setParameter('nom', 'plop');
+        $this->assertTrue(0 == $query->getSingleScalarResult());
+    }
 }
