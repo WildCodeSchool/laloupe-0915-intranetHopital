7403339ed3fe3490d7f938a691fc1b4d095a5e78
Erwan HAQUET <ehaquet.dev@gmail.com>
2015-12-17 15:46:17 +0100

    Debuging the dates

--- src/IuchBundle/Admin/UserAdmin.php
+++ src/IuchBundle/Admin/UserAdmin.php
@@ -153,7 +153,7 @@ class UserAdmin extends Admin
                 ))
                 ->add('dateOfBirth', 'date', array(
                     'widget' => 'single_text',
-                    'format' => 'yyyy-MM-dd',
+                    'format' => 'dd/MM/yyyy',
                 ))
                 ->add('enabled', null, array(
                     'required' => false
@@ -179,15 +179,11 @@ class UserAdmin extends Admin
             ->with('Dates arrivée & départ')
                 ->add('date_entree', 'date', array(
                     'label' => 'Date d\'entrée',
-                    'widget' => 'single_text',
-                    'format' => 'yyyy-MM-dd',
-                    'required' => true
-                ))
+                    'widget' => 'choice',
+                    ))
                 ->add('date_sortie', 'date', array(
                     'label' => 'Date de sortie',
-                    'widget' => 'single_text',
-                    'format' => 'yyyy-MM-dd',
-                    'required' => false
+                    'widget' => 'choice',
                 ))
                 ->add('raison_sortie', 'choice', array(
                     'choices' => array('' => '', 'MATERNITE' => 'MATERNITE', 'RETRAITE' => 'RETRAITE', 'ARRET MALADIE' => 'ARRET MALADIE', 'FIN DE CONTRAT' => 'FIN DE CONTRAT'),
