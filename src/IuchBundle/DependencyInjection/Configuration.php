a4d5458bac6db1a34229e232cc55412dff2f7b55
Lucie Mannechez <lucie.mannechez@gmail.com>
2016-01-07 17:33:27 +0100

    Add parameters to enabled or not the trombinoscope

--- src/IuchBundle/DependencyInjection/Configuration.php
+++ src/IuchBundle/DependencyInjection/Configuration.php
@@ -17,11 +17,11 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
+
+        $treeBuilder = new TreeBuilder();
 
         return $treeBuilder;
     }
