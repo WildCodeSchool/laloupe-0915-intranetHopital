05d41210b4bcb4980af6e3245ebf886afb82f23c
Lucie Mannechez <lucie.mannechez@gmail.com>
2016-01-11 10:55:52 +0100

    Remove unused use

--- src/Application/Sonata/UserBundle/Command/DesactivateUserCommand.php
+++ src/Application/Sonata/UserBundle/Command/DesactivateUserCommand.php
@@ -4,7 +4,6 @@
 namespace Application\Sonata\UserBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
