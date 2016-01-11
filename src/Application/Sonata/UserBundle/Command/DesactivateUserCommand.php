133efc41f4ce9f3b4e369693f8abf76a5bd10516
Lucie Mannechez <lucie.mannechez@gmail.com>
2016-01-11 10:35:52 +0100

    Add command for cron tache to desactivate out users

---
+++ src/Application/Sonata/UserBundle/Command/DesactivateUserCommand.php
@@ -0,0 +1,28 @@
+<?php
+// src/Application/Sonata/UserBundle/Command/DesactivateUserCommand.php
+
+namespace Application\Sonata\UserBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DesactivateUserCommand extends ContainerAwareCommand {
+
+    protected function configure()
+    {
+        $this
+        ->setName('user:out:desactivate')
+        ->setDescription('Desactivate out user')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
+        $nb = $em->getRepository('ApplicationSonataUserBundle:User')->desactivateOutUser();
+
+        $output->writeln(sprintf('Desactivate %d out user', $nb));
+    }
+}
